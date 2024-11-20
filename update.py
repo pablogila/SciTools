54430290b7f483e8a847bf0400117dfcb1d0c09c
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2024-11-20 15:19:05 +0100

    Automatic push on 2024-11-20 15:19 with InputMaker v0.5.1

--- update.py
+++ update.py
@@ -6,14 +6,14 @@ import inputmaker as im
 
 # Link my Obsidian notes with the final files
 dict_files = {
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/SciTools.md")         : "README.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/QuantumESPRESSO.md")  : "QuantumESPRESSO.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/CASTEP.md")           : "CASTEP.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/cif2cell.md")         : "cif2cell.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/ASE.md")              : "ASE.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/Zotero.md")           : "Zotero.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/StructuralDB.md")     : "StructuralDB.md",
-    im.get_file("/home/pablo/Documents/obsidian/Work ⚛️/Instruments/TorrentTrackers.md")  : "TorrentTrackers.md",
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/SciTools.md'        : 'README.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/QuantumESPRESSO.md' : 'QuantumESPRESSO.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/CASTEP.md'          : 'CASTEP.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/cif2cell.md'        : 'cif2cell.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/ASE.md'             : 'ASE.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/Zotero.md'          : 'Zotero.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/StructuralDB.md'    : 'StructuralDB.md',
+    '/home/pablo/Documents/obsidian/Work ⚛️/Instruments/TorrentTrackers.md' : 'TorrentTrackers.md',
 }
 
 # Dict to fix Obsidian wikilinks
